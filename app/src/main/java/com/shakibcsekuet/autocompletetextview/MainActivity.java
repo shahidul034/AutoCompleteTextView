89bbe0d6afd8395981617cdb0fec2c34d6b83508
SHAKIB <shakibkhanrock@gmail.com>
2017-10-29 14:13:51 +0600

    Auto Complete TextView

---
+++ app/src/main/java/com/shakibcsekuet/autocompletetextview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.shakibcsekuet.autocompletetextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] countryNameList = {"India", "China", "Australia", "New Zealand", "England", "Pakistan"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initiate an auto complete text view
+        AutoCompleteTextView simpleAutoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.simpleAutoCompleteTextView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, countryNameList);
+
+        simpleAutoCompleteTextView.setAdapter(adapter);
+        simpleAutoCompleteTextView.setThreshold(1);//start searching from 1 character
+        simpleAutoCompleteTextView.setAdapter(adapter);   //set the adapter for displaying country name list
+    }
+
+
+}
